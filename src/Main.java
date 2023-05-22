fc3db7dac9fc4e3e46ebd348b58488e4d78dfe53
Bedirhan Yazici <bedirhanyazici64@gmail.com>
2023-05-23 02:07:32 +0300

    Taksimetre programi olusturuldu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int km;
+        double perKM = 2.20, total, startPrice = 10;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Mesafeyi kilometre cinsinden giriniz: ");
+        km = input.nextInt();
+
+        total = (km * perKM);
+        total += startPrice;
+
+        total = total < 20 ? 20 : total;
+
+        System.out.print("Ödenecek tutar: " + total);
+
+
     }
 }
